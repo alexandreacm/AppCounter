12ffe9c26bfab6f5f169fa3d890562c056ece180
Alexandre carvalho Marques <lexandreacm.marques@gmail.com>
2022-03-26 14:21:55 -0300

    test husky 2.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community'
+  env: {
+    es2021: true
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react','@react-native-community'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }]
+  }
 };
